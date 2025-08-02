9200d879c9c007eb3361bc744b646c7f2be4f678
salzta79 <salztamannix@gmail.com>
2025-08-02 18:38:49 +1000

    Create App.jsx

---
+++ public/src/src/src/App.jsx
@@ -0,0 +1,17 @@
+```jsx
+import React from "react";
+import IdentifierApp from "./IdentifierApp";
+
+export default function App() {
+  return (
+    <IdentifierApp
+      appName="PlantID"
+      themeColor="#2a9d8f"
+      placeholderImage="https://upload.wikimedia.org/wikipedia/commons/a/a4/Plant-leaf.jpg"
+      description="Identify any plant instantly and get detailed care tips."
+    />
+  );
+}
+```
+
+---
